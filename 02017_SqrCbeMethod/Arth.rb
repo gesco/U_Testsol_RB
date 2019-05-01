e7293db2b0e56e4e2ff2735fddbba6e8ddcae851
gesco <gescobedoz@gmail.com>
2019-04-30 22:27:37 -0500

    Task 3: Square and Cube Method

---
+++ 02017_SqrCbeMethod/Arth.rb
@@ -0,0 +1,23 @@
+class M
+
+  def add_num(a, b)
+    return a + b
+  end
+
+  def sub_num(a, b)
+    return a - b
+  end
+
+  def mult_num(a, b)
+    return a * b
+  end
+
+  def square_num(a)
+    return a * a
+  end
+
+  def cube_num(a)
+    return a * a * a
+  end
+
+end
---
+++ 02017_SqrCbeMethod/Main.rb
@@ -0,0 +1,9 @@
+require './Arth'
+obj = M.new
+
+a = 2
+b = 3
+
+puts obj.square_num(a) + obj.cube_num(b)
+# 4 + 27
+# 31
